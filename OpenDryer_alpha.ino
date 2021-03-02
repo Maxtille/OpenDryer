fa20e89083353a2e4fad686f41321b3033fff925
Maxtille <41472832+Maxtille@users.noreply.github.com>
2021-03-02 16:01:51 +0100

    Add files via upload

--- OpenDryer_alpha.ino
+++ OpenDryer_alpha.ino
@@ -13,7 +13,9 @@
 #define DHTPINM 9 // broche ou l'on a branche le capteurM
 #define DHTPINL 10 // broche ou l'on a branche le capteurL
 #define DHTTYPE DHT22
+#define LED_RENEW 12 //broche ou l'on connecte la led qui indique le renouvellement automatique
 #define LED_OK LED_BUILTIN // broche ou l'on connecte la led qui indique par son clignottement que le programme tourne normalement
+#define RELAY 1   // broche ou l'on a branche le relais et la led qui indique l'état du relais
 
 DHT dht_h(DHTPINH, DHTTYPE);//déclaration du capteurH
 DHT dht_m(DHTPINM, DHTTYPE);//déclaration du capteurM
@@ -25,6 +27,7 @@ void setup() {
   pinMode(DHTPINH, INPUT);
   pinMode(DHTPINM, INPUT);
   pinMode(DHTPINL, INPUT);
+  pinMode(RELAY, OUTPUT);
   Serial.begin(9600);
   Serial.print("Initialisation");
   dht_h.begin();
@@ -33,6 +36,9 @@ void setup() {
   Serial.println(" ok");
   pinMode(LED_OK, OUTPUT);
 }
+
+unsigned int loopCount =0;    //compteur du nombre de fois que loop a été executé
+bool isRenewing = false;      //true = cycle de renouvellement automatique en cours
 
 void loop() {
   //déclaration et initialisation des variables
@@ -43,12 +49,12 @@ void loop() {
   // La lecture du capteur prend 250ms
   // Les valeurs lues peuvent etre vieilles de jusqu'a 2 secondes (le capteur est lent)
   //mais ici on va se contenter d'une lecture toute les 10 secondes
-  humH = dht_h.readHumidity();//on lit l'hygrometrieH
-  tempH = dht_h.readTemperature();//on lit la temperatureH en celsius (par defaut)
-  humM = dht_m.readHumidity();//on lit l'hygrometrieM
-  tempM = dht_m.readTemperature();//on lit la temperatureM en celsius (par defaut)
-  humL = dht_l.readHumidity();//on lit l'hygrometrieL
-  tempL = dht_l.readTemperature();//on lit la temperatureL en celsius (par defaut)
+  humH = dht_h.readHumidity();              //on lit l'hygrometrieH
+  tempH = dht_h.readTemperature();          //on lit la temperatureH en celsius (par defaut)
+  humM = dht_m.readHumidity();              //on lit l'hygrometrieM
+  tempM = dht_m.readTemperature();          //on lit la temperatureM en celsius (par defaut)
+  humL = dht_l.readHumidity();              //on lit l'hygrometrieL
+  tempL = dht_l.readTemperature();          //on lit la temperatureL en celsius (par defaut)
 
 
   //On verifie si la lecture a echoue, si oui on quitte la boucle pour recommencer.
@@ -65,19 +71,36 @@ void loop() {
 
   // Calcul de l'indice de temperature en Celsius
   //float indiceH = dht_h.computeHeatIndex(tempH, humH, false);
+  
 
   //Calcul des moyennes (et des minimas maximas)
   tempMoy = ( tempH + tempM + tempL ) / 3 ;
   humMoy = ( humH + humM + humL ) / 3 ;
 
+
   //évaluation de l'état du système
+  switch (loopCount)
+  {
+    case 1080 : //après 1080 loop il devrait s'etre passé 3h
+    {//renouvellement automatique de l'air une fois toute les 3h (relais ON pendant 5 min)
+      digitalWrite(RELAY, HIGH);
+      isRenewing = true;
+      loopCount =0;
+      break;
+    }
+    case 30 : //si renouvellement, stop
+    {
+      if(isRenewing)
+        digitalWrite(RELAY, LOW);
+      isRenewing = false;
+      break;
+    }
+    default:
+    {//maintien à une hygro stable et réglable via un potentiomètre analogique (45-70)
+      
+    }
+  }
   
-  //maintien à une hygro stable et réglable via un potentiomètre analogique (45-70)
-  //et
-  //renouvellement automatique de l'air une fois toute les 3h (relais ON pendant 5 min)
-
-  
-
   //Affichages :
   Serial.print("Température moyenne : ");
   Serial.print(tempMoy);
@@ -85,11 +108,15 @@ void loop() {
   Serial.print("Humidité moyenne : ") ;
   Serial.print(humMoy);
   Serial.println(" %");
+  if(isRenewing)
+    digitalWrite(LED_RENEW,HIGH);
+  else
+    digitalWrite(LED_RENEW,LOW);
+  
 
   //Sauvegardes des données et évènnements sur sd
 
 
-  
 
  //Timing de la boucle + LED 
   do
@@ -100,5 +127,6 @@ void loop() {
   }while(millis()<(StartLoopTime + 9890 ));
   do
   {delay(1);}while(millis()<(StartLoopTime + 10000)) ;
+  loopCount ++;
 
 }
